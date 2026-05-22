ac26581360e652998750cdc269494fd316631725
sroyal-jpg <sroyal@ucsb.edu>
2026-05-22 16:16:12 -0700

    got electricity to a good place to revise metal production, playing with that module again

--- modules/electricity_cost.R
+++ modules/electricity_cost.R
@@ -9,28 +9,32 @@
 #' @param day
 
 
-############# REVISED VERSION WITH MONTH AND DAY AS THE INPUTS #######
-
-days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+### MAKE THIS NOT A FOR LOOP
 
 cost_of_electric <- function (month, day) {
   
-  for (d in days) {
-    
     # higher summer prices
     if (month %in% c(6, 7, 8)) {
       
-      # runs this electricity cost if it IS during the summer
-      electricity_cost <- runif(n = 100, min = 20, max = 40)} 
+      # runs this electricity cost if it's during the summer
+      electricity_cost <- runif(n = 1, min = 26, max = 40)} 
     
     # runs this electricity cost if it's not during the summer
-    else { electricity_cost <- runif(n = 100, min = 10, max = 25)
+    else { electricity_cost <- runif(n = 1, min = 10, max = 25)
     }
     
-    electricity <- ifelse(electricity_cost[n] >= 16,"high","low"
-    )
-    print(day)
-    print(electricity_cost[n])
-    print(electricity)
+  # makes the cost of electricity on weekends 50% cheaper
+  if (day %in% c("Saturday", "Sunday")) {
+    electricity_cost <- (electricity_cost * 0.5)
   }
+  
+    electricity <- ifelse(electricity_cost >= 16,"high","low")
+    
+    # what the function returns as outputs
+    return(
+      data.frame(
+        day = day,
+        month = month,
+        electricity_cost = electricity_cost,
+        electricity = electricity))
 }
--- modules/metal_production.R
+++ modules/metal_production.R
@@ -13,15 +13,53 @@
 
 #' @param production rate (%)
 
+
+# can I put the data frame inside of this module?
+
+# make different parts of the data frame based on if electricity is high or low
+
 # function definition
 
-metal_production = function(metal_ore, recycled_metal, production_rate) {
+metal_production = function(electricity) {
   
   produced_metal = (metal_ore + recycled_metal) * (production_rate)
   
   return(produced_metal)
   
 }
+
+###################### REVISED VERSION ########
+
+metal_production <- function(electricity) {
+  
+  metal <- c("steel", "aluminum", "copper")
+  
+  metal_ore <- data.frame(
+    metal = metal,
+    mass = runif(length(metal), 1, 100)
+  )
+  
+  # production rate depends on electricity
+    metal_ore$production_rate <- ifelse(
+    electricity == "high",
+    runif(length(metal), 0, 0.5),
+    runif(length(metal), 0.6, 1)
+  )
+  
+  # recycling rate depends on electricity
+  metal_ore$recycling_rate <- ifelse(
+    electricity == "high",
+    runif(length(metal), 0.3, 0.7),
+    runif(length(metal), 0.6, 1)
+  )
+  
+  metal_ore$collection_rate <- runif(length(metal), 0, 1)
+  
+  return(metal_ore)
+  
+}
+
+
 
 
 
