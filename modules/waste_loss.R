b3bca9fbfa88c5df875b260fb7cbcf2ac2db3625
Nathan Lau <nathaniel.lau@gmail.com>
2026-05-23 09:05:46 -0700

    Scrap not recycled added to waste metal

--- modules/waste_loss.R
+++ modules/waste_loss.R
@@ -3,18 +3,20 @@
 
 # CONTRACT
 
-# inputs: primary metal (MT), collection rate (%)
+# inputs: primary metal (MT), collection rate (%), recycled metal (MT), recycling rate (%)
 # output: waste metal (MT)
 
 # parameters
 ## collection rate
+## recycling rate
 
 #' @param collection rate (%)
+#' @param recycling rate (%)
 
 # function definition
-waste_loss = function(produced_metal, collection_rate) {
+waste_loss = function(produced_metal, collection_rate, recycled_metal, recycling_rate) {
   
-  waste_metal = (produced_metal * (1-collection_rate))
+  waste_metal = ((produced_metal * (1 - collection_rate) + (recycled_metal * (1 - recycling_rate))))
   
   return(waste_metal)
   
