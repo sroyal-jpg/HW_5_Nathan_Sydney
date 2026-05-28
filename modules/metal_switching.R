858010c9c111e4cf506bed31a84824c726bae1be
sroyal-jpg <sroyal@ucsb.edu>
2026-05-28 14:19:16 -0700

    changed script to have a week and weekend version

--- modules/metal_switching.R
+++ modules/metal_switching.R
@@ -3,46 +3,94 @@
 
 ### THIS IS THE FOR LOOP
 
-## run each module one row at a time
-produced_metal <- numeric(nrow(metal_ore))  # pre-allocate empty results vectors
-collected_scrap <- numeric(nrow(metal_ore))
-recycled_metal <- numeric(nrow(metal_ore))
-waste <- numeric(nrow(metal_ore))
+#################### SEPARATE INTO WEEK AND WEEKEND LOOPS
+
+## WEEK LOOP
+week_produced_metal <- numeric(nrow(metal_ore))     # pre-allocate empty results vectors
+week_collected_scrap <- numeric(nrow(metal_ore))
+week_recycled_metal <- numeric(nrow(metal_ore))
+week_waste <- numeric(nrow(metal_ore))
 
 for (i in 1:nrow(metal_ore)) {
-  produced_metal[i] <- metal_production(
-    metal_ore     = metal_ore$mass[i],
-    recycled_metal = recycled_metal[i],
-    production_rate = metal_ore$production_rate[i]
+  
+  week_produced_metal[i] <- metal_production(
+    metal_ore = metal_ore$mass[i],
+    recycled_metal = week_recycled_metal[i],
+    production_rate = metal_ore$week_production_rate[i]
   )
   
-  collected_scrap[i] <- collection_scrap(
-    produced_metal = produced_metal[i],
+  week_collected_scrap[i] <- collection_scrap(
+    produced_metal = week_produced_metal[i],
     collection_rate = metal_ore$collection_rate[i]
   )
   
-  recycled_metal[i] <- scrap_recycling(
-    collected_scrap = collected_scrap[i],
-    recycling_rate = metal_ore$recycling_rate[i]
-  ) 
+  week_recycled_metal[i] <- scrap_recycling(
+    collected_scrap = week_collected_scrap[i],
+    recycling_rate = metal_ore$week_recycling_rate[i]
+  )
   
-  waste[i] <- waste_loss(
-    produced_metal = produced_metal[i],
+  week_waste[i] <- waste_loss(
+    produced_metal = week_produced_metal[i],
     collection_rate = metal_ore$collection_rate[i],
-    recycled_metal = recycled_metal[i],
-    recycling_rate = metal_ore$recycling_rate[i],
+    recycled_metal = week_recycled_metal[i],
+    recycling_rate = metal_ore$week_recycling_rate[i],
     mass = metal_ore$mass[i]
   )
 }
 
-# create an intermediary data frame for new variables of transformed metals
-metal_transformed <- data.frame(
+# create intermediary dataframe
+week_metal_transformed <- data.frame(
   metal = metal,
-  produced_metal,
-  collected_scrap,
-  recycled_metal,
-  waste
+  week_produced_metal,
+  week_collected_scrap,
+  week_recycled_metal,
+  week_waste
+)
+
+## WEEKEND LOOP
+weekend_produced_metal <- numeric(nrow(metal_ore))     # pre-allocate empty results vectors
+weekend_collected_scrap <- numeric(nrow(metal_ore))
+weekend_recycled_metal <- numeric(nrow(metal_ore))
+weekend_waste <- numeric(nrow(metal_ore))
+
+for (i in 1:nrow(metal_ore)) {
+  
+  weekend_produced_metal[i] <- metal_production(
+    metal_ore = metal_ore$mass[i],
+    recycled_metal = weekend_recycled_metal[i],
+    production_rate = metal_ore$weekend_production_rate[i]
+  )
+  
+  weekend_collected_scrap[i] <- collection_scrap(
+    produced_metal = weekend_produced_metal[i],
+    collection_rate = metal_ore$collection_rate[i]
+  )
+  
+  weekend_recycled_metal[i] <- scrap_recycling(
+    collected_scrap = weekend_collected_scrap[i],
+    recycling_rate = metal_ore$weekend_recycling_rate[i]
+  )
+  
+  weekend_waste[i] <- waste_loss(
+    produced_metal = weekend_produced_metal[i],
+    collection_rate = metal_ore$collection_rate[i],
+    recycled_metal = weekend_recycled_metal[i],
+    recycling_rate = metal_ore$weekend_recycling_rate[i],
+    mass = metal_ore$mass[i]
+  )
+}
+
+# create intermediary dataframe
+weekend_metal_transformed <- data.frame(
+  metal = metal,
+  weekend_produced_metal,
+  weekend_collected_scrap,
+  weekend_recycled_metal,
+  weekend_waste
 )
 
 # merge transformed metals and metal ore data frames
-metal_full_cycle <- merge(metal_ore, metal_transformed)
+week_metal_full_cycle <- merge(metal_ore, week_metal_transformed)
+weekend_metal_full_cycle <- merge(metal_ore, weekend_metal_transformed)
+
+
