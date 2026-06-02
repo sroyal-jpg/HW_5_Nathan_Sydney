27db63890be807fa0158b367ddf9cf32cd6b1f7c
sroyal-jpg <sroyal@ucsb.edu>
2026-06-02 09:07:11 -0700

    fixed documentation for collection function according to instructions in class

--- modules/collection_scrap.R
+++ modules/collection_scrap.R
@@ -1,13 +1,11 @@
 
-# COLLECTION OF SCRAP METAL MODULE
-# THIS ONE IS USING THE ROXYGEN PACKAGE
-
-#' Scrap Collected from Produced Metal
+#' collection_scrap
 #' 
 #' This function determines the amount of scrap metal collected after metal was produced by a given collection rate for a metal ore type
-#' @param collection_rate Percent of metal collected
+#' @param collection_rate Percent (between 0-1) of metal collected
 #' @param produced_metal Amount of metal produced (MT)
 #' @return collected_scrap (MT)
+#' @author Sydney Royal
 
 # function definition
 collection_scrap = function(produced_metal, collection_rate) {
