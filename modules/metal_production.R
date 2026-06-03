768843d1a2ee261a788aa396ac2cf4fb877d2fc5
Nathan Lau <nathaniel.lau@gmail.com>
2026-06-02 19:13:00 -0700

    Added roxygen to metal production module

--- modules/metal_production.R
+++ modules/metal_production.R
@@ -1,16 +1,10 @@
-
-# METAL PRODUCTION MODULE
-
-# CONTRACT
-
-# inputs: metal ore, recycled metal
-# output: primary metal
-
-# parameters
-#' @param production rate (%)
-
-
-# can I put the data frame inside of this module?
+#' Metal Production Module
+#'
+#' @param production_rate Percentage of metal ore transformed into produced, primary metal in Mt.
+#' @param metal_ore Starting mass of metal ore in Mt.
+#' @param recycled_metal Amount of collected scrap metal recycled in Mt.
+#' @return produced_metal Amount of metal produced from metal ore and recycled metal in Mt.
+#' @author Sydney
 
 # make different parts of the data frame based on if electricity is high or low
 
@@ -27,7 +21,3 @@ metal_production = function(metal_ore, recycled_metal, production_rate) {
   return(produced_metal)
   
 }
-
-
-
-
