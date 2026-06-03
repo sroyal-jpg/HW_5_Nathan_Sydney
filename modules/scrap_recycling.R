aeedd0ed3b9c4d08478b3a7930210b54ec76da37
Nathan Lau <nathaniel.lau@gmail.com>
2026-06-02 19:04:37 -0700

    Added roxygen to recycling module

--- modules/scrap_recycling.R
+++ modules/scrap_recycling.R
@@ -1,20 +1,14 @@
 
 # RECYCLING OF SCRAP METAL MODULE
 
-# CONTRACT
-
-# inputs: scrap metal (Mt)
-# output: recycled metal (Mt)
-
-# parameters
-#' @param recycling rate (%)
-
-# function definition
+#' @param recycling_rate Percentage of collected scrap metal that is successfully recycled.
+#' @param collected_scrap Amount of produced metal that is collected as scrap in Mt.
+#' @return recycled_metal Amount of collected scrap metal that is recycled in Mt.
+#' @author Nathan
 
 scrap_recycling = function(collected_scrap, recycling_rate) {
   
   recycled_metal = (collected_scrap * recycling_rate)
   
   return(recycled_metal)
-  
 }
